5567ee930a6065e12e3ce201d63da54ec38946d2
Stefan Krasser <stefan.krasser@edu.fh-joanneum.at>
2018-01-21 11:31:41 +0100

    joinGame fetches newest .bin file

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxJoin.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxJoin.scala
@@ -1,15 +1,27 @@
 package at.fhj.swengb.apps.battleship.jfx
 
+import java.io.File
 import java.net.URL
 import java.util.ResourceBundle
 import javafx.fxml.{FXML, Initializable}
 import javafx.scene.Scene
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+import java.util
+import javafx.beans.InvalidationListener
+import javafx.collections.{FXCollections, ListChangeListener, ObservableArray, ObservableList}
+import javafx.scene.control.ListView
+
+import scala.collection.mutable.ListBuffer
+
 
 
 class BattleShipFxJoin extends Initializable {
 
+ @FXML var gamesToJoin: ListView[String] = _
+
  @FXML def next(): Unit = {
-  //BattleShipFxApp.display(BattleShipFxApp.loadEditGame,BattleShipFxApp.loadMain)
+  BattleShipFxApp.display(BattleShipFxApp.loadEditGame,BattleShipFxApp.loadMain)
  }
 
  @FXML def back(): Unit = {
@@ -17,12 +29,27 @@ class BattleShipFxJoin extends Initializable {
  }
 
 
- override def initialize(location: URL, resources: ResourceBundle): Unit = {
+ override def initialize(location: URL, resources: ResourceBundle): Unit = init()
+
+
+ def getFiles(): List[File] = {
+  val d = new File("battleship")
+  if (d.exists && d.isDirectory)
+   d.listFiles.filter(x => x.isFile && x.getName.contains(".bin")).toList
+  else
+   List[File]()
 
  }
 
 
+ def init(): Unit = {
+  val files = getFiles()
+  val names = files.map(x => (x.getName, x.lastModified()))
+  val recentGame = names.max
 
+  BattleShipFxApp.setFilename(recentGame._1)
 
+  println(recentGame)
+ }
 
 }
