ab9380988fdb50dca12c6eccd8944638695fb94c
Christoph Mali <Christoph.Mali@ssi-schaefer.com>
2018-01-23 17:04:08 +0100

    add a case class for HighscoreLine

--- battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/model/HighscoreLine.scala
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/model/HighscoreLine.scala
@@ -4,40 +4,33 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import javafx.beans.property.{SimpleIntegerProperty, SimpleStringProperty}
 
+case class HighscoreL(date: Date, winner: String, gameName: String, numShots: Int)
+
 class HighscoreLine {
 
-    val date: SimpleStringProperty = new SimpleStringProperty()
-    val winner: SimpleStringProperty = new SimpleStringProperty()
-    val gameName: SimpleStringProperty = new SimpleStringProperty()
-    val numOfShots: SimpleIntegerProperty = new SimpleIntegerProperty()
+  val date: SimpleStringProperty = new SimpleStringProperty()
+  val winner: SimpleStringProperty = new SimpleStringProperty()
+  val gameName: SimpleStringProperty = new SimpleStringProperty()
+  val numOfShots: SimpleIntegerProperty = new SimpleIntegerProperty()
 
-    private var game: GameRound = _
+  def setDate(date: Date): Unit = this.date.set(new SimpleDateFormat("yyyy/MM/dd").format(date))
 
-    def setDate(date: Date): Unit = this.date.set(new SimpleDateFormat("yyyy/MM/dd").format(date))
+  def setWinner(win: String): Unit = this.winner.set(win)
 
-    def setWinner(win: String): Unit = this.winner.set(win)
+  def setGameName(name: String): Unit = this.gameName.set(name)
 
-    def setGameName(name: String): Unit = this.gameName.set (name)
+  def setNumOfShots(clicks: Int): Unit = this.numOfShots.set(clicks)
 
-    def setNumOfShots(clicks: Int): Unit = this.numOfShots.set(clicks)
+}
 
-    def setGameRound(gameReplay: GameRound): Unit = {
-      game = gameReplay
-    }
+object HighscoreLine {
+  def apply(hl: HighscoreL): HighscoreLine = {
+    val line = new HighscoreLine
+    line.setDate(hl.date)
+    line.setWinner(hl.winner)
+    line.setGameName(hl.gameName)
+    line.setNumOfShots(hl.numShots)
+    return line
 
-    def getGameRound: GameRound = return game
   }
-
-  object HighscoreLine {
-    def apply(game: GameRound): HighscoreLine = {
-      val line = new HighscoreLine
-      line.setDate(game.getDate())
-      line.setWinner(game.getWinner())
-      line.setGameName(game.gameName)
-      line.setNumOfShots(game.getNumOfShots())
-      line.setGameRound(game)
-
-      return line
-
-    }
 }
